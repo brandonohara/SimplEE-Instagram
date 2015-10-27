27745d3316e0eacf630a02ccd4389a86ed1f13e5
Brandon O'Hara <brandon@brandonohara.com>
2015-10-26 23:10:55 -0600

    Restructure File System

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define("INSTAGRAM_DEVELOPER_ID", "");
-
-	require_once(PATH_THIRD."simplee_instagram/models/EE_Object.php");
-	require_once(PATH_THIRD."simplee_instagram/models/Instagram_Post.php");
-	require_once(PATH_THIRD."simplee_instagram/models/Instagram_User.php");
-	require_once(PATH_THIRD."simplee_instagram/models/Instagram_Comment.php");
-?>
